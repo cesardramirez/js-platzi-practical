9ed2287ca2d291a6c5d79c6d33f84ced4adba32e
cesardramirez <cesardavid89@gmail.com>
2023-03-11 23:54:59 -0500

    feat(read): reading html from js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello, world!')
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const pAll = document.querySelectorAll('p');
+const input = document.querySelector('input');
+const input1 = document.getElementsByTagName('input');
+const paragraph = document.querySelector('.paragraph');
+const paragraph2 = document.getElementsByClassName('paragraph');
+const pid = document.querySelector('#pid');
+const pid2 = document.getElementById('pid');
+
+// En cada variable se pueden visualizar los atributos de cada una, para así manipularlas mejor como DOM.
+console.log({
+    h1,
+    p,
+    pAll,  // NodeList (3) [p, p.paragraph, p#pid]
+    input,
+    input1,  // HTMLCollection [input]
+    paragraph,  // p.paragraph
+    paragraph2,
+    pid,  // p#pid
+    pid2  // HTMLCollection [p.paragraph]
+});
+
+console.log(input.value);  // 175623
